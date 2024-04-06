f873d069ed08484c51d9806f523bf2097e8fdaa1
MilenaPellegrino <milenapellegrinoch@gmail.com>
2024-04-06 17:05:12 -0300

    Add gitignore, partialsum, etc

---
+++ CoderForces/Contest/2_Round173/bit++.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fore(i,a,b) for(int i=(a);i<(b);i++)
+#define forr(i, a, b) for(int i=(b);i>(a);i--)
+#define forn(e,c) for(const auto &e : (c))
+#define db(x) cout<<#x<< " = "<<(x)<<endl
+#define sz(x) ((int)x.size())
+#define all(x) (x).begin(),(x).end()
+#define pb push_back
+#define pp pop_back
+#define fst first
+#define snd second
+#define str string
+#define pri(x) cout << (x) << "\n"
+#define mset(a,v) memset((a),(v),sizeof(a))
+#define FIN ios::sync_with_stdio(0);cin.tie(0);cout.tie(0); 
+using ll = long long;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using vii = vector<int>;
+using vll = vector<ll>;
+using vpi = vector<pii>;
+template<class T>ostream&operator<<(ostream&o,vector<T>const&v){o<<"[ ";for(auto const&x:v)o<<x<<" ";return o<<"]";}
+
+void solve(){
+	int n; cin>>n; 
+	int ans = 0;
+	fore(i, 0, n){
+		char a, b, c; cin>>a>>b>>c; 
+		if(a == '+' || c == '+') ans ++;
+		else ans --;
+	}
+	pri(ans);
+}
+ 
+int main(){
+    FIN; 
+    int t = 1;
+    //int t; cin>>t; 
+    while(t--){
+			solve();
+	}
+    return 0;
+}
